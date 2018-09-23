5a0b43eec173e3cf0a11f447c01c81ef7d921162
kebaliva <iuk1990.2013@gmail.com>
2018-09-23 07:56:14 +0300

    we

---
+++ шар/шар/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace шар
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите диаметр шара");
+            double d = Double.Parse(Console.ReadLine());
+            double S = Math.PI * Math.Pow(d, 2);
+            double V = Math.PI * Math.Pow(d, 3) / 6;
+            Console.WriteLine($"Площадь поверхности шара {S}, объем шара {V}");
+        }
+    }
+}
